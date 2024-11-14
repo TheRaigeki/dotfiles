e609fff3b6d3a0997857f8a1eaa8068c538510d5
RobinRuf <robin.ruf@hotmail.com>
2024-11-15 00:30:47 +0100

    renamed and reorganized files

---
+++ .config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,31 @@
+return {
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup({})
+    end,
+  },
+
+  -- Show Emojis in autocomplete, if something like ':smile' was written
+  {
+    "nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+
+  -- HTML Autotags
+  {
+    "nvim-ts-autotag",
+     config = function()
+       require('nvim-ts-autotag').setup()
+     end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+  }
+}
