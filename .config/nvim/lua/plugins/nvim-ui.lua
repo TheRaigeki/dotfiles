e0e0949569e890d123bf30ec97d3de6db7e7492a
RobinRuf <robin.ruf@hotmail.com>
2024-12-09 23:49:10 +0100

    extended snacks.nvim config

--- .config/nvim/lua/plugins/nvim-ui.lua
+++ .config/nvim/lua/plugins/nvim-ui.lua
@@ -52,12 +52,6 @@ return {
       opts.presets.lsp_doc_border = true
     end,
   },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 3000,
-    },
-  },
 
   -- bufferline
   {
@@ -107,12 +101,25 @@ return {
     end,
   },
 
-  -- Logo
+  -- snacks.nvim
     {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
         opts = {
+            bigfile = { enabled = true },
+            notifier = {
+                enabled = true,
+                timeout = 3000,
+            },
+            quickfile = { enabled = true },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
+            styles = {
+                notification = {
+                    wo = { wrap = true }
+                }
+            },
             dashboard = {
                 preset = {
                                 
@@ -137,6 +144,19 @@ return {
                     },
                 }
             },
+        },
+        keys = {
+            { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+            { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+            { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+            { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+            { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+            { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+            { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+            { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+            { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+            { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+            { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
         },
     }
 }
