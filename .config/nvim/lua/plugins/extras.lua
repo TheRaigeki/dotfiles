c969a631bc9d7c640f712aa463ebb0537f59fb16
RobinRuf <robin.ruf@hotmail.com>
2024-12-18 02:29:40 +0100

    removed unnecessary nvim-cmp

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,25 +1,16 @@
 return {
-  -- Incremental rename
-  {
-    "smjonas/inc-rename.nvim",
-    config = function()
-      require("inc_rename").setup({})
-    end,
-  },
+    -- Incremental rename
+    {
+        "smjonas/inc-rename.nvim",
+        config = function()
+            require("inc_rename").setup({})
+        end,
+    },
 
-  -- Show Emojis in autocomplete, if something like ':smile' was written
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-
-  -- HTML Autotags
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-  }
+    -- HTML Autotags
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true,
+    },
 }
