d5238bf3e296d5a3fd7d9c3f1f5152a8c4ecf42a
RobinRuf <robin.ruf@hotmail.com>
2025-03-10 02:31:46 +0100

    modifed configs and removed border of telescope

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,29 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparant = true,
-    }
-  end,
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+        return {
+            transparant = true,
+            on_highlights = function(hl, c)
+                local prompt = "#2d3149"
+                hl.TelescopeNormal = {
+                    bg = c.bg_dark,
+                    fg = c.fg_dark,
+                }
+                hl.TelescopeBorder = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopePreviewTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopeResultsTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+            end,
+        }
+    end,
 }
