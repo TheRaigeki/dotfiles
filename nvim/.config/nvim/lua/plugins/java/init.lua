2697f1c4dc922ea10de7577ca1d457506a666629
RobinRuf <robin.ruf@hotmail.com>
2025-10-06 02:09:14 +0200

    updated java config

--- nvim/.config/nvim/lua/plugins/java/init.lua
+++ nvim/.config/nvim/lua/plugins/java/init.lua
@@ -1,23 +1,15 @@
 return {
-  'nvim-java/nvim-java',
-  config = false,
-  dependencies = {
-    {
-      'neovim/nvim-lspconfig',
-      opts = {
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
-          },
-        },
-        setup = {
-          jdtls = function()
-            require('java').setup({
-              -- Your custom nvim-java configuration goes here
-            })
-          end,
-        },
-      },
+    "nvim-java/nvim-java",
+    ft = { "java" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mason-org/mason.nvim",
     },
-  },
-}
+    config = function()
+      require("java").setup({
+      })
+
+      require("lspconfig").jdtls.setup({
+      })
+    end,
+  }
