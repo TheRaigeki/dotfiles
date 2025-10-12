77fe8e445b65d977cde4080b915ee8c94b0a68b4
RobinRuf <robin.ruf@hotmail.com>
2025-10-12 19:50:41 +0200

    changed theme from solarized osaka to tokyonight

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,57 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparant = true,
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        -- Highlight Color in Visual Mode
-        hl.Visual = {
-          fg = "NONE",
-          bg = "#1e3842",
-        }
-      end,
-    }
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        -- night, moon, storm, day (light theme)
+        style = "night",
+        transparent = true,
+        lualine_bold = true,
+        styles = {
+          comments  = { italic = true },
+          keywords  = { italic = false },
+          functions = {},
+          variables = {},
+          sidebars  = "transparent", -- make sidebars transparent
+          floats    = "transparent", -- make floats (incl. Telescope) transparent
+        },
+        on_highlights = function(hl, c)
+          -- Ensure all generic floats are transparent + keep a border
+          hl.NormalFloat = { bg = "NONE" }
+          hl.FloatBorder = { bg = "NONE", fg = c.blue }
+
+          local prompt = true;
+
+          -- Telescope: fully transparent panes, readable border, subtle selection
+          hl.TelescopeNormal        = { bg = "NONE", fg = c.fg }
+          hl.TelescopeBorder        = { bg = "NONE", fg = c.blue }
+
+          hl.TelescopePromptNormal  = { bg = "NONE", fg = c.fg }
+          hl.TelescopePromptBorder  = { bg = "NONE", fg = c.blue }
+          hl.TelescopePromptPrefix  = { bg = "NONE", fg = c.blue }
+          hl.TelescopePromptTitle   = { bg = "NONE", fg = c.blue }
+
+          hl.TelescopeResultsNormal = { bg = "NONE", fg = c.fg }
+          hl.TelescopeResultsBorder = { bg = "NONE", fg = c.blue }
+          hl.TelescopeResultsTitle  = { bg = "NONE", fg = c.blue }
+
+          hl.TelescopePreviewNormal = { bg = "NONE", fg = c.fg }
+          hl.TelescopePreviewBorder = { bg = "NONE", fg = c.blue }
+          hl.TelescopePreviewTitle  = { bg = "NONE", fg = c.blue }
+
+          -- Selection inside Telescope list (keeps it visible without filling the whole pane)
+          hl.TelescopeSelection     = { bg = c.bg_highlight, fg = c.fg }
+
+          -- change text color of unused variables to comment color, because the original color was to hard to read with transparent bg
+          hl["@lsp.mod.unused"]  = { fg = c.comment, italic = true, strikethrough = false }
+          hl.DiagnosticUnnecessary = { fg = c.comment, italic = true, strikethrough = false }
+
+          -- visual selection (if highlight color is too hard to see, uncomment the line below)
+          -- hl.Visual = { fg = "NONE", bg = "#33467C" }
+        end,
+      }
+    end,
+  },
 }
