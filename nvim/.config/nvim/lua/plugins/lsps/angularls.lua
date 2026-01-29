f0e98d95fc5d047ec0447c5dad11258afabe93fb
Robin <robin.ruf@hotmail.com>
2026-01-29 20:24:35 +0100

    fixed angularlsp issue
    now, I should be able to open an angular project whereever I want

--- nvim/.config/nvim/lua/plugins/lsps/angularls.lua
+++ nvim/.config/nvim/lua/plugins/lsps/angularls.lua
@@ -13,14 +13,20 @@ local function get_angular_core_version(root_dir)
   
   local json = vim.json.decode(f:read('*a')) or {}
   f:close()
-  
-  local version = (json.dependencies or {})['@angular/core'] or ''
+
+  -- Check both dependencies and devDependencies
+  local version = (json.dependencies or {})['@angular/core']
+    or (json.devDependencies or {})['@angular/core']
+    or ''
   return version:match('%d+%.%d+%.%d+') or ''
 end
 
 return {
   cmd = function(dispatchers, config)
-    local root_dir = config.root or fn.getcwd()
+    local root_dir = config.root_dir or config.root or fn.getcwd()
+
+    -- Debug: uncomment to verify version detection
+    -- vim.notify('angularls root: ' .. root_dir .. ' | version: ' .. get_angular_core_version(root_dir), vim.log.levels.INFO)
     
     -- Hardcode known paths, to prevent expensive fs operations
     local ngserver_path = fn.stdpath('data') .. '/mason/packages/angular-language-server/node_modules/@angular/language-server/bin/ngserver'
@@ -47,5 +53,27 @@ return {
   end,
   
   filetypes = { 'typescript', 'html', 'htmlangular' },
-  root_markers = { 'angular.json', 'nx.json' },
+
+  root_dir = function(bufnr, on_dir)
+    local util = require('lspconfig.util')
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+
+    -- First, try standard upward search for angular.json
+    local standard_root = util.root_pattern('angular.json', 'nx.json')(fname)
+    if standard_root then
+      on_dir(standard_root)
+      return
+    end
+
+    -- Fallback: search upward for a dir containing frontend/webapp/angular.json
+    local dir = vim.fs.dirname(fname)
+    while dir and dir ~= '/' do
+      local angular_path = dir .. '/frontend/webapp/angular.json'
+      if vim.uv.fs_stat(angular_path) then
+        on_dir(dir .. '/frontend/webapp')
+        return
+      end
+      dir = vim.fs.dirname(dir)
+    end
+  end,
 }
